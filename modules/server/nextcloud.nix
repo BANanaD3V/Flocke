9544628c341da7953f264ea1fb7af9a5e69e221a
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2024-06-11 09:42:04 +0300

    fix(nextcloud): disable not on server

--- modules/server/nextcloud.nix
+++ modules/server/nextcloud.nix
@@ -1,27 +1,31 @@
 {
   pkgs,
+  lib,
   config,
   ...
 }: let
   package = "nextcloud29";
 in {
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.${package};
-    extraApps = {
-      inherit (pkgs."${package}Packages".apps) notes bookmarks maps tasks spreed previewgenerator phonetrack memories calendar contacts forms;
+  options.server.nextcloud.enable = lib.mkEnableOption "Enable nextcloud" // {default = config.server.enable;};
+  config = lib.mkIf config.server.nextcloud.enable {
+    services.nextcloud = {
+      enable = true;
+      package = pkgs.${package};
+      extraApps = {
+        inherit (pkgs."${package}Packages".apps) notes bookmarks maps tasks spreed previewgenerator phonetrack memories calendar contacts forms;
+      };
+      hostName = "nextcloud.banana.is-cool.dev";
+      config.adminpassFile = "/etc/nextcloud-admin-pass";
+      https = true;
+      settings = {
+        trusted_domains = [
+          "nextcloud.banana.is-cool.dev"
+        ];
+      };
     };
-    hostName = "nextcloud.banana.is-cool.dev";
-    config.adminpassFile = "/etc/nextcloud-admin-pass";
-    https = true;
-    settings = {
-      trusted_domains = [
-        "nextcloud.banana.is-cool.dev"
-      ];
+    services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+      forceSSL = true;
+      enableACME = true;
     };
-  };
-  services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-    forceSSL = true;
-    enableACME = true;
   };
 }
