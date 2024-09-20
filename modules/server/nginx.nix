1a926e5256337966c1d8a08e56371e15ccada2c2
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2024-09-20 16:35:48 +0300

    feat(nginx): apply recommended settings

--- modules/server/nginx.nix
+++ modules/server/nginx.nix
@@ -6,37 +6,45 @@
 }: {
   options.server.nginx.enable = lib.mkEnableOption "Enable nginx" // {default = config.server.enable;};
   config = lib.mkIf config.server.nginx.enable {
-    services.nginx.enable = true;
-    services.nginx.clientMaxBodySize = "512M";
-    services.nginx.virtualHosts = {
-      "invidious.banana.is-cool.dev" = {
-        addSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:9000";
+    services.nginx = {
+      enable = true;
+      clientMaxBodySize = "512M";
+      recommendedTlsSettings = true;
+      recommendedZstdSettings = true;
+      recommendedOptimisation = true;
+      recommendedGzipSettings = true;
+      recommendedProxySettings = true;
+      recommendedBrotliSettings = true;
+      virtualHosts = {
+        "invidious.banana.is-cool.dev" = {
+          addSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:9000";
+          };
         };
-      };
-      "searxng.banana.is-cool.dev" = {
-        addSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:7666";
+        "searxng.banana.is-cool.dev" = {
+          addSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:7666";
+          };
         };
-      };
 
-      "0x0.banana.is-cool.dev" = {
-        addSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:5555";
+        "0x0.banana.is-cool.dev" = {
+          addSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:5555";
+          };
         };
-      };
 
-      "navidrome.banana.is-cool.dev" = {
-        addSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:4533";
+        "navidrome.banana.is-cool.dev" = {
+          addSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:4533";
+          };
         };
       };
     };
